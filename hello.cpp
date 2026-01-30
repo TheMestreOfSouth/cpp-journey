bf197575f1d0594817136fb285e19567d3643605
enzofishy <itsenzogabriel@gmail.com>
2026-01-29 21:29:24 -0300

    Add hello.cpp with Hello World example
    O clássico dos clássicos. Se não fizer esse, a maldição do programador cai sobre você e nada funciona. É só um código básico que cospe "Hello World" na tela pra eu ver que o compilador não tá de sacanagem comigo.

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Este eh o meu primeiro codigo em C++ rodando no Manus!" << std::endl;
+    return 0;
+}
